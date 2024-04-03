15cf0a3369579897c6150f90e869d25052bcb933
David <davidagamez04@gmail.com>
2024-04-03 17:27:59 -0500

    feature: user validated

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,9 @@
 import NextAuth, { type NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials'
-
+import bcryptjs from 'bcryptjs'
 import { z } from 'zod'
+
+import prisma from './lib/prisma';
 
 export const authConfig: NextAuthConfig = {
     pages: {
@@ -14,15 +16,22 @@ export const authConfig: NextAuthConfig = {
                     .object({ email: z.string().email(), password: z.string().min(6) })
                     .safeParse(credentials)
 
-                    if( !parsedCredentials.success ) return null;
+                if (!parsedCredentials.success) return null;
 
-                    const { email, password } = parsedCredentials.data
+                const { email, password } = parsedCredentials.data
 
-                    return null
+                const user = await prisma.user.findUnique({ where: { email: email.toLocaleLowerCase() } });
+                
+                if( !user ) return null
+                if( !bcryptjs.compareSync( password, user.password ) ) return null;
+
+                const { password: _, ...rest } = user;
+                console.log({rest})
+                return rest;
             }
         })
     ]
 };
 
 
-export const { signIn, signOut } = NextAuth( authConfig )
+export const { signIn, signOut } = NextAuth(authConfig)
